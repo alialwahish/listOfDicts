23306aa4c6423763d153be8dfab101fc56c50778
Ali Bayati <bayati.ali@icloud.com>
2018-05-01 16:50:34 -0500

    initial commit

--- nameInDict.py
+++ nameInDict.py
@@ -1,9 +1,24 @@
-students = [
+
+
+users = {
+ 'Students': [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'},
      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
      {'first_name' : 'KB', 'last_name' : 'Tonel'}
-]
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
 
-for name in students:  
-    print name["first_name"],name["last_name"]        
+for kv in users:
+    # if kv == "Instructors":
+    print kv
+    tim=1
+    for i in users[kv]:
+        print tim,"-",i["first_name"],i["last_name"],"-",len(i["first_name"])+len(i["last_name"])
+        tim+=1
+     
+            
